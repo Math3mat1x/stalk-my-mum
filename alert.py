873635bf1df0925d79fae4b0e65cf1d4d54418df
Maxime Corne <maxime.corne@protonmail.com>
2021-07-30 01:55:44 +0200

    Function to alert the user.

---
+++ alert.py
@@ -0,0 +1,11 @@
+def alert(friend):
+    """
+    Alert the user using a service of you choice. 
+    This has to be filled manually.
+
+    Input:
+        friend (str) : the email or phone_number of the friend near you.
+    """
+
+    # Example
+    print("{} is near you.".format(friend))
