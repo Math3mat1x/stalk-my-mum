8a936510659d790cb6c0055184ddecc071e22b02
Maxime Corne <maxime.corne@protonmail.com>
2021-08-08 17:52:52 +0200

    asyncio suppport for infos.py

--- stalk_my_mum/infos.py
+++ stalk_my_mum/infos.py
@@ -1,5 +1,7 @@
 from math import radians, sin, cos, atan2, sqrt
-import requests
+# import requests
+import aiohttp
+import asyncio
 import polyline
 import json
 
@@ -24,17 +26,21 @@ class Coordinates:
 
         return round(earth_radius * c, ndigits=1)
 
-    def travel(self):
+    async def travel(self):
         a = (self.lat1, self.lng1)
         b = (self.lat2, self.lng2)
 
         url = "http://router.project-osrm.org/table/v1/driving/polyline(" + \
                 polyline.encode([b, a]) + ")"
 
-        response = requests.get(url, {"sources": 0, "destinations": 1, \
-                "annotations": "duration,distance"})
 
-        response = json.loads(response.text)
+        payload = {"sources": 0, "destinations": 1, "annotations": "duration,distance"}
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=payload) as response:
+                response = await response.text()
+
+        response = json.loads(response)
     
         return {"duration": response["durations"][0][0],\
                 "distance": response["distances"][0][0]}
@@ -43,21 +49,36 @@ class Coordinates:
         return ";".join([",".join([str(i), str(j)]) for i,j in [(self.lat1, self.lng1), (self.lat2, self.lng2)]])
 
     @property
-    def distance(self):
+    async def distance(self):
         return self.find_distance()
 
     @property
-    def time(self):
+    async def time(self):
+        print("Let’s start")
         index = self._get_index_record()
         if not index in self.record.keys():
-            self.record[index] = self.travel()
+            self.record[index] = await self.travel()
 
         return self.record[index]["duration"]
 
     @property
-    def travel_distance(self):
+    async def travel_distance(self):
         index = self._get_index_record()
         if not index in self.record.keys():
-            self.record[index] = self.travel()
+            self.record[index] = await self.travel()
 
         return self.record[index]["distance"]
+
+async def test():
+    a = (5.6757872631206885, 47.52696804616896)
+    b = (5.659161387857111, 47.50893169258636)
+    c = (47.10636973360826, 5.954746857889816)
+
+    test_1 = Coordinates(a,b)
+    test_2 = Coordinates(a, c)
+
+    return await asyncio.gather(*(i.time for i in [test_1, test_2]))
+
+if __name__ == "__main__":
+    result = asyncio.run(test())
+    print(result)
